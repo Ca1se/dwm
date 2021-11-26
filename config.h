889acef63947c8d50084f5e04922c0d87cdde362
caseshot <blame773@gmail.com>
2021-11-26 16:58:44 +0000

    2021.11.26

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray             = 1;   /* 0 means no systray */
-static const char *fonts[]          = { "JetBrains Mono:size=10" };
+static const char *fonts[]          = { "JetBrains Mono:size=10", "source han sans cn:size=10" };
 static const char dmenufont[]       = "JetBrains Mono:size=10";
 static const char col_gray1[]            = "#222222";
 static const char col_gray2[]            = "#444444";
@@ -32,15 +32,14 @@ static const unsigned int alphas[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const Rule rules[] = {
+static const Rule rules[1];
+// {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-};
+// };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
